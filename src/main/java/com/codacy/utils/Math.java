8b1ed773ccbaac84f2ac1e12fe43f5df34ef4dfd
Luis Ventura <luis.ventura@codacy.com>
2023-11-07 10:26:46 +0000

    asdadsa

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -21,6 +21,9 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
             return y - x;
         } else {
             System.out.println("I am covered!");
