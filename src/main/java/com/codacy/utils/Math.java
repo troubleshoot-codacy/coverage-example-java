c4f446e86e67fab0678907351c71bc038b3fa049
Joao Machado <machado@codacy.com>
2023-11-17 10:43:06 +0000

    Update Math.java

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -16,10 +16,13 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
+            System.out.println("Am I am coveredss?");
+            System.out.println("Am I am coveredss?");
             return y - x;
         } else {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
+            System.out.println("I am covered!!!");
             return x + y;
         }
     }
