1c35e35ba157a239ae0b30e6b60eb6140e1a7bb5
franciscoazevedo <francisco.azevedo@codacy.com>
2025-01-02 18:10:44 +0000

    change change change

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -9,15 +9,28 @@ public class Math {
     }
 
     /**
-     * Adds 2 numbers, unless there is a magic number on the second argument or 4th arguments is another push
+     * Adds 2 numbers, unless there is a magic number on the second argument or 4th arguments is another push wiht changedssss
      */
     public int magicAdd(int x, int y) {
         if (y == magicNumber) {
             System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             return y - x;
         } else {
+            System.out.println("I am covered!");
+            System.out.println("I am covered!");
+            System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
             return x + y;
