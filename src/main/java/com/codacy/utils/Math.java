2159606c8da3b103029a16e420a6b6e22e41ce1f
franciscoazevedo <francisco.azevedo@codacy.com>
2025-01-14 15:30:53 +0000

    new commit

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -17,6 +17,13 @@ public class Math {
             System.out.println("Am I am covered? AM I? ");
             System.out.println("Am I am covered? AM I? ");
             System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
