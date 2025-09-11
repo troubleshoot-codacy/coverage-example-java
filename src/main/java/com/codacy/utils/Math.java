824adf7cb5ae9ff663fffa583769e6a4a0e25bfa
manufacturist <15235526+manufacturist@users.noreply.github.com>
2025-09-11 13:14:02 +0300

    one more issue

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -28,7 +28,8 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
-            return abs(y - x);
+
+            throw new RuntimeException();
         } else if (y != magicNumber){
             System.out.println("I am covered!");
             System.out.println("I am covered!");
