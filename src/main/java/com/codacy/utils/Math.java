69f0f6c39d2dbb728750274018ab29736ebbe4a1
manufacturist <15235526+manufacturist@users.noreply.github.com>
2025-09-18 18:08:48 +0300

    trigger issue again

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -26,9 +26,7 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
-<<<<<<< Updated upstream
             return y - x;
-=======
 
             throw new RuntimeException();
         } else if (y != magicNumber){
@@ -39,7 +37,6 @@ public class Math {
             System.out.println("I am covered!");
 
             throw new RuntimeException();
->>>>>>> Stashed changes
         } else {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
