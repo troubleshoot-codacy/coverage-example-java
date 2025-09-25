19cb5ed84af602c7a8198be0f4eb6c6bc5e9465d
Milosz Jakubanis <milosz.jakubanis@codacy.com>
2025-09-25 12:30:14 +0100

    wip

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -36,9 +36,15 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
 
             throw new RuntimeException();
         } else if (y != magicNumber){
+            System.out.println("I am covered!");
+            System.out.println("I am covered!");
+            System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
