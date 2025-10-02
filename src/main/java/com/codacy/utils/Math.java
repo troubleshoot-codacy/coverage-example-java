61384c3a7299e4a18f2cac8181278ef226b9c81c
Milosz Jakubanis <milosz.jakubanis@codacy.com>
2025-10-02 11:14:10 +0100

    wip

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -28,6 +28,8 @@ public class Math {
             System.out.println("Am I am covered? AM I? ");
             System.out.println("Am I am covered? AM I? ");
             System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
+            System.out.println("Am I am covered? AM I? ");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
